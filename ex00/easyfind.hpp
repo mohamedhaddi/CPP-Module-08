2c6dae84170640513293cd48673fd24bcc77639c
Mohamed Haddi <mhaddi@student.1337.ma>
2022-04-01 21:33:30 +0000

    use STL's find in easyfind

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -13,20 +13,15 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+#include <algorithm>
+
 template<typename T>
 typename T::iterator easyfind(T & container, int value)
 {
 	typename T::iterator it = container.begin();
 	typename T::iterator end = container.end();
 
-	while (it != end)
-	{
-		if (*it == value)
-			return (it);
-		it++;
-	}
-
-	return (container.end());
+	return std::find(it, end, value);
 }
 
 
